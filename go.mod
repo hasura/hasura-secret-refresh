28528cc072bc87995313f11b238918fb9486c20f
Harish Nair <harishnair@hasura.io>
2023-09-04 15:18:24 +0530

    initialize idanywhere provider

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/hasura/hasura-secret-refresh
 
 go 1.20
 
-require github.com/aws/aws-secretsmanager-caching-go v1.1.2
+require (
+	github.com/aws/aws-secretsmanager-caching-go v1.1.2
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+)
 
 require (
 	github.com/aws/aws-sdk-go v1.44.287 // indirect
---
+++ provider/idanywhere.go
@@ -0,0 +1,45 @@
+package provider
+
+import (
+	"net/url"
+	"time"
+
+	cache "github.com/patrickmn/go-cache"
+)
+
+type IdAnywhere struct {
+	cache               *cache.Cache
+	awsSm               *AwsSecretsManager
+	certificateSecretId string
+	oAuthUrl            url.URL
+	oAuthClientId       string
+	jwtClaimMap         map[string]string
+}
+
+func (provider IdAnywhere) GetSecret(requestConfig RequestConfig) (secret string, err error) {
+	return
+}
+
+func CreateIdAnywhereProvider(
+	secretsManagerCacheTtl time.Duration,
+	certificateSecretId string,
+	oAuthUrl url.URL,
+	oAuthClientId string,
+	jwtClaimMap map[string]string,
+) (provider IdAnywhere, err error) {
+	secresManagerProvider, err := CreateAwsSecretsManagerProvider(secretsManagerCacheTtl)
+	if err != nil {
+		return
+	}
+	cacheDefaultExpiration := 5 * time.Minute
+	cachePurgeFrequency := time.Hour
+	cache := cache.New(cacheDefaultExpiration, cachePurgeFrequency)
+	return IdAnywhere{
+		awsSm:               &secresManagerProvider,
+		cache:               cache,
+		certificateSecretId: certificateSecretId,
+		oAuthUrl:            oAuthUrl,
+		oAuthClientId:       oAuthClientId,
+		jwtClaimMap:         jwtClaimMap,
+	}, nil
+}
