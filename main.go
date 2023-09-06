3387a9926c64524c16c408a272cf6e60b93f834a
Harish Nair <harishnair@hasura.io>
2023-09-07 00:36:32 +0530

    make log level configurable

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
 	configPath := flag.String(server.ConfigFileCliFlag, server.ConfigFileDefaultPath, server.ConfigFileCliFlagDescription)
+	logLevel := flag.String("log", "info", "set log level: debug, info, error")
 	flag.Parse()
 	initLogger := logger.With().
 		Str("config_file_path", *configPath).
@@ -24,5 +25,23 @@ func main() {
 	if err != nil {
 		initLogger.Fatal().Err(err).Msg("Unable to parse config file")
 	}
+	zLogLevel := getLogLevel(*logLevel, logger)
+	zerolog.SetGlobalLevel(zLogLevel)
 	server.Serve(config, logger)
+}
+
+func getLogLevel(level string, logger zerolog.Logger) zerolog.Level {
+	levelMap := map[string]zerolog.Level{
+		"debug": zerolog.DebugLevel,
+		"info":  zerolog.InfoLevel,
+		"error": zerolog.ErrorLevel,
+	}
+	zLevel, ok := levelMap[level]
+	if !ok {
+		logger.Info().Msg("Setting log level to 'info'")
+		return zerolog.InfoLevel
+	} else {
+		logger.Info().Msgf("Setting log level to '%s'", level)
+		return zLevel
+	}
 }
