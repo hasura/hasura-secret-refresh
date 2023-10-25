c5aec62658dd8cee4e5075461880563a6b10fb60
Harish Nair <harishnair@hasura.io>
2023-10-25 13:41:42 +0530

    add provider type to config

--- server/config.go
+++ server/config.go
@@ -32,14 +32,24 @@ func ParseConfig(rawConfig map[string]interface{}, logger zerolog.Logger) (confi
 			logger.Error().Msgf("Failed to convert config to required type")
 			return
 		}
-		if k == aws_secrets_manager {
+		providerTypeI, found := providerData["type"]
+		if !found {
+			logger.Error().Msgf("Provider type not specified. Ensure that the type is specified for every provider using the 'type' field")
+			return
+		}
+		providerType, ok := providerTypeI.(string)
+		if !ok {
+			logger.Error().Msgf("'type' of provider must be a string value")
+			return
+		}
+		if providerType == aws_secrets_manager {
 			sublogger := logger.With().Str("provider_name", aws_secrets_manager).Logger()
 			provider_, err = awsSm.Create(providerData, sublogger)
 			if err != nil {
 				sublogger.Err(err).Msgf("Error creating provider")
 				return
 			}
-		} else if k == aws_sm_oauth {
+		} else if providerType == aws_sm_oauth {
 			sublogger := logger.With().Str("provider_name", aws_sm_oauth).Logger()
 			provider_, err = awsSmOauth.Create(providerData, sublogger)
 			if err != nil {
