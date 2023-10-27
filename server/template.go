ae9f8b061ae9ccfc8e29c00ac804ffaef098622a
Harish Nair <harishnair@hasura.io>
2023-10-27 16:27:21 +0530

    support simple json in template

--- server/template.go
+++ server/template.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"regexp"
@@ -23,7 +24,30 @@ func getHeaderFromTemplate(
 	headerValTemplate := split[1]
 	headerValTemplate = strings.TrimSpace(headerValTemplate)
 	headerVal = regex.ReplaceAllStringFunc(headerValTemplate, func(s string) string {
-		return substituteWith
+		s = strings.TrimSpace(s)
+		s = strings.TrimLeft(s, "##")
+		s = strings.TrimRight(s, "##")
+		res := jsonTemplate(s, substituteWith)
+		return res
 	})
 	return
+}
+
+func jsonTemplate(jsonTemplate, substituteWith string) string {
+	jsonPath := strings.Split(jsonTemplate, ".")
+	if len(jsonPath) < 2 {
+		return substituteWith
+	}
+	jsonParsed := make(map[string]string)
+	err := json.Unmarshal([]byte(substituteWith), &jsonParsed)
+	if err != nil {
+		return ""
+	}
+	jsonKey := strings.TrimSpace(jsonPath[1])
+	val, ok := jsonParsed[jsonKey]
+	if !ok {
+		return ""
+	}
+	val = strings.TrimSpace(val)
+	return val
 }
