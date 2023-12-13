84e8f8b44d9f5d7cd8673d23fb704f1678bf9630
Harish Nair <harishnair@hasura.io>
2023-12-13 14:24:05 +0530

    if json parsing fails, do not attempt further

--- template/template.go
+++ template/template.go
@@ -18,41 +18,45 @@ type Template struct {
 var regex = regexp.MustCompile("##(.*?)##")
 
 func (t Template) Substitute(with string) string {
+	canContinue := true
 	result := regex.ReplaceAllStringFunc(string(t.Templ), func(s string) string {
 		s = strings.TrimSpace(s)
 		s = strings.TrimLeft(s, "##")
 		s = strings.TrimRight(s, "##")
-		res := jsonTemplate(s, with, t.Logger)
+		var res string
+		if canContinue {
+			res, canContinue = jsonTemplate(s, with, t.Logger)
+		}
 		return res
 	})
 	return result
 }
 
-func jsonTemplate(jsonTemplate, substituteWith string, logger zerolog.Logger) string {
+func jsonTemplate(jsonTemplate, substituteWith string, logger zerolog.Logger) (res string, canContinue bool) {
 	jsonPath := strings.Split(jsonTemplate, ".")
 	if len(jsonPath) < 2 {
-		return substituteWith
+		return substituteWith, true
 	}
 	jsonParsed := make(map[string]interface{})
 	err := json.Unmarshal([]byte(substituteWith), &jsonParsed)
 	if err != nil {
 		logger.Err(err).Msg("Unable to parse secret as a JSON")
 		logger.Debug().Err(err).Msgf("Unable to parse secret as a JSON: %s", substituteWith)
-		return ""
+		return "", false
 	}
 	jsonKey := strings.TrimSpace(jsonPath[1])
 	val, ok := jsonParsed[jsonKey]
 	if !ok {
 		logger.Error().Msgf("Key %s not found in secret", jsonKey)
 		logger.Debug().Msgf("Key %s not found in secret %s", jsonKey, substituteWith)
-		return ""
+		return "", true
 	}
 	if reflect.ValueOf(val).Kind() == reflect.Map {
 		logger.Error().Msgf("Nested JSON is not supported in secrets")
 		logger.Debug().Msgf("Nested JSON is not supported, secret: %s", substituteWith)
-		return ""
+		return "", true
 	}
 	valS := fmt.Sprintf("%v", val)
 	valS = strings.TrimSpace(valS)
-	return valS
+	return valS, true
 }
