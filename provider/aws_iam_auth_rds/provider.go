836226e03b2e0c9130964acf2128811e5a5a51cd
Karthik Venkateswaran <karthik@hasura.io>
2025-02-10 18:37:33 +0530

    close database after the database is pinged

--- provider/aws_iam_auth_rds/provider.go
+++ provider/aws_iam_auth_rds/provider.go
@@ -59,6 +59,12 @@ func (provider *AWSIAMAuthRDSFile) checkDSNConnectivity(dsn string) error {
 		provider.logger.Error().Err(err).Msg("failed to ping the database with the generated token")
 		return err
 	}
+	// close the conn
+	err = db.Close()
+	if err != nil {
+		provider.logger.Error().Err(err).Msg("failed to close the database after an initial ping")
+		return err
+	}
 	return nil
 }
 
